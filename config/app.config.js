c629ed85d42a3e53a1ff2dd1b2f9bffa9ba61690
Alcantara47 <derrickforcha360@gmail.com>
2023-06-23 18:31:42 +0100

    first commit

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG={
+    DB:'mongodb+srv://derrickforcha360:5rlUuHUgPv2rPhz3@react-backendportfolio.ofwwatp.mongodb.net/ReactFrontendProject?retryWrites=true&w=majority'
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
